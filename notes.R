ab8904f46767d0462b5a57d4a5981fba23063061
maczokni <maczokni@gmail.com>
2019-07-15 10:46:20 +0100

    dataviz

---
+++ notes.R
@@ -0,0 +1,10 @@
+blogdown::new_site(theme = "koirand/pulp", theme_example = TRUE)
+blogdown::new_site(theme = "gcushen/hugo-academic")
+
+blogdown::new_site()
+blogdown::install_theme("koirand/pulp", force = TRUE, theme_example = TRUE)
+blogdown::serve_site()
+blogdown::new_post(title="Data Visualisation Summer School", 
+         ext = getOption("blogdown.ext", ".Rmd"))
+
+
