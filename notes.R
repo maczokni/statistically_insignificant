aea308e5f28ca9b86a97e6c760289bcbd1c61855
maczokni <maczokni@gmail.com>
2019-08-14 10:14:02 +0100

    I am wackily editing html i will do this properly in a bit...

--- notes.R
+++ notes.R
@@ -4,7 +4,7 @@ blogdown::new_site(theme = "gcushen/hugo-academic")
 blogdown::new_site()
 blogdown::install_theme("koirand/pulp", force = TRUE, theme_example = TRUE)
 blogdown::serve_site()
-blogdown::new_post(title="Webscraping and some sampling & mapping in R", 
+blogdown::new_post(title="kaki", 
          ext = getOption("blogdown.ext", ".Rmd"))
 
 
