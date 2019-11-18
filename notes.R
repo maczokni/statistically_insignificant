79ac71a3126b0cd2a0c5cf564eb68e3e6f042534
maczokni <maczokni@gmail.com>
2019-11-18 16:15:58 +0000

    asc blog post

--- notes.R
+++ notes.R
@@ -4,10 +4,8 @@ blogdown::new_site(theme = "gcushen/hugo-academic")
 blogdown::new_site()
 blogdown::install_theme("koirand/pulp", force = TRUE, theme_example = TRUE)
 blogdown::serve_site()
-blogdown::new_post(title="Halloween MCR", 
+blogdown::new_post(title="ASC 2019", 
          ext = getOption("blogdown.ext", ".Rmd"))
 
 
 
-library(blogdown)
-install.packages("rnaturalearthhires", repos = "http://packages.ropensci.org", type = "source")
