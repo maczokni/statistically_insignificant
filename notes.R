453cf29be016b5b5999737b581ffd48aefd30a18
maczokni <maczokni@gmail.com>
2020-01-19 10:13:22 +0000

    wirral school blog post

--- notes.R
+++ notes.R
@@ -4,7 +4,7 @@ blogdown::new_site(theme = "gcushen/hugo-academic")
 blogdown::new_site()
 blogdown::install_theme("koirand/pulp", force = TRUE, theme_example = TRUE)
 blogdown::serve_site()
-blogdown::new_post(title="ASC 2019", 
+blogdown::new_post(title="Crime Analysis for Schools", 
          ext = getOption("blogdown.ext", ".Rmd"))
 
 
