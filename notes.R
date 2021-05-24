77f3e2a297f771fe324710c5df5111877bacad1e
maczokni <maczokni@gmail.com>
2021-05-24 12:20:58 +0100

    sage video post

--- notes.R
+++ notes.R
@@ -5,6 +5,6 @@ blogdown::new_site()
 blogdown::install_theme("koirand/pulp", force = TRUE)
 blogdown::serve_site()
 blogdown::stop_server()
-blogdown::new_post(title="A response to: on the safety of women", ext = ".Rmarkdown", subdir = 'blog')
+blogdown::new_post(title="Minimizing Misrepresentation in Spatial Data Visualizations", ext = ".Rmarkdown", subdir = 'blog')
 
 blogdown::check_site()
