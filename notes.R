5936666cf2303c62edbdfbf43c90ed009fd2ca99
maczokni <maczokni@gmail.com>
2021-07-12 13:49:48 +0100

    knife crime paper blog post

--- notes.R
+++ notes.R
@@ -5,6 +5,6 @@ blogdown::new_site()
 blogdown::install_theme("koirand/pulp", force = TRUE)
 blogdown::serve_site()
 blogdown::stop_server()
-blogdown::new_post(title="Minimizing Misrepresentation in Spatial Data Visualizations", ext = ".Rmarkdown", subdir = 'blog')
+blogdown::new_post(title="Understanding Knife Crime and Trust in Police with Young People in East London", ext = ".Rmarkdown", subdir = 'blog')
 
 blogdown::check_site()
