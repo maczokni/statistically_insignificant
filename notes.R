5f97bff2477b7c7e96e02fb0d338b0791d84bf70
Reka Solymosi <maczokni@users.noreply.github.com>
2026-03-04 13:03:21 +0000

    resquant blog update

--- notes.R
+++ notes.R
@@ -5,7 +5,7 @@ blogdown::new_site()
 blogdown::install_theme("koirand/pulp", force = TRUE)
 blogdown::serve_site()
 blogdown::stop_server()
-blogdown::new_post(title="Can chatGPT pass my statistics module?", ext = ".Rmarkdown", subdir = 'blog')
+blogdown::new_post(title="AI Tools for Researchers: A Practical Introduction", ext = ".Rmarkdown", subdir = 'blog')
 
 blogdown::check_site()
 blogdown::hugo_version()
